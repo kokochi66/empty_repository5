d68a4afe31a92e6c63313459bab7dc261cd12a1a
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-03-19 01:23:29 +0900

    NettyApplication

---
+++ src/main/java/com/kokochi/game/web/netty/NettyApplication.java
@@ -0,0 +1,13 @@
+package com.kokochi.game.web.netty;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyApplication implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
