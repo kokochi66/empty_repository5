ed35332304d58e3f825aecd0d90ab435f29b1bb5
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-03-19 01:37:54 +0900

    Update NettyApplication.java
    NettyApplication 업데이트

--- src/main/java/com/kokochi/game/web/netty/NettyApplication.java
+++ src/main/java/com/kokochi/game/web/netty/NettyApplication.java
@@ -1,26 +1,37 @@
 package com.kokochi.game.web.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
-public class NettyApplication implements CommandLineRunner {
+public class NettyApplication {
+    private NettyServer nettyServer;
 
-    private static final int PORT = 1234;
-
-    @Bean
-    public NettyServerHandler nettyServerHandler() {
-        return new NettyServerHandler();
+    public NettyApplication() {
     }
 
-    @Override
-    public void run(String... args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(NettyApplication.class, args);
-        NettyServer nettyServer = new NettyServer();
-        nettyServer.start(PORT);
+    }
+
+    public void run() throws Exception {
+        ApplicationContext context = new AnnotationConfigApplicationContext();
+        nettyServer = new NettyServer();
+        nettyServer.start(1234);
     }
 }
